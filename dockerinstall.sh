09d4d924249a5285dcd6d20b997beee0f46caa43
hanumanth9 <47772318+hanumanth9@users.noreply.github.com>
2021-09-25 13:28:32 +0900

    Update dockerinstall.sh

--- dockerinstall.sh
+++ dockerinstall.sh
@@ -1,1 +1,12 @@
-sudo apt-get -y install docker.io 
+sudo apt-get -y install docker.io
+DIR="docker-install"
+if [ -d "$DIR" ]; then
+  # Take action if $DIR exists. #
+  git pull https://github.com/hanumanth9/docker-install.git
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Error: ${DIR} not found. Can not continue."
+  git pull https://github.com/hanumanth9/docker-install.git
+  cd docker-install
+  sudo docker-compose up -d
+fi
